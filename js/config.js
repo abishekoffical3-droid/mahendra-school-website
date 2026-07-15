e35fe4454022c775de3c3f34daf0c2c336987cb7
abishekoffical3-droid <abishekoffical3@gmail.com>
2026-07-15 18:56:56 +0545

    Project structure setup

---
+++ js/config.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+export const supabase = createClient(
+  "https://jisoljwfzmamblbccemr.supabase.co",
+  "sb_publishable__Iss5xP0CG2DyJ0PmrwKvg__Prj5Kec"
+);
